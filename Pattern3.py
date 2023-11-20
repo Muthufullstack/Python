f3b07fd5c004bb96a9dfed06d32a537160a4b902
Muthufullstack <138384683+Muthufullstack@users.noreply.github.com>
2023-11-20 22:57:46 +0530

    Update Pattern3.py

--- Pattern3.py
+++ Pattern3.py
@@ -3,3 +3,14 @@ for i in range(1,m + 1,1):
     for j in range(1,m+1,1):
         print("*",end=" ")
     print()
+
+'''
+ m=5
+Output:
+      *****
+      *****
+      *****
+      *****
+      *****
+      
+'''
