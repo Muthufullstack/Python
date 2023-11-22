d757350969c6ca5ac0d78d41437eb65a13c91962
Muthufullstack <138384683+Muthufullstack@users.noreply.github.com>
2023-11-22 22:29:15 +0530

    Update Pattern4.py

--- Pattern4.py
+++ Pattern4.py
@@ -1,9 +1,9 @@
 n= int(input("Enter a number : "))
 for i in range(1,n+1,1):
 	for j in range(1,n-i+1,1):
-		   print(end=" ")
+		     print(end=" ")
 	for k in range(1,i+1):
-	    print("*",end=" ")
+	      print("*",end=" ")
 	print()
 
 '''
