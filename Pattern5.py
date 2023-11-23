00b0515f6a30a32f1dabc6ef9da065ce9ea08fe4
Muthufullstack <138384683+Muthufullstack@users.noreply.github.com>
2023-11-23 22:46:48 +0530

    Update Pattern5.py

--- Pattern5.py
+++ Pattern5.py
@@ -1,8 +1,8 @@
 n=int(input("Enter a number : "))
 for i in range(n,0,-1):
-	for j in range(1,n-i):
+	for j in range(0,n-i):
 		print(end=" ")
-	for j in range(1,i):
+	for j in range(0,i):
 		print("*",end=" ")
 	print()
 '''
