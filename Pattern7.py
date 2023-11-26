68e05c44c3cd5b403834941c269a163b22420b7b
Muthufullstack <138384683+Muthufullstack@users.noreply.github.com>
2023-11-26 19:46:55 +0530

    Update

---
+++ Pattern7.py
@@ -0,0 +1,8 @@
+r = int(input("Enter a number : "))
+c = int(input("Enter a number : "))
+for i in range(r):
+      if i == 0 or i == r - 1:
+           print("*" * c)
+      else:
+           print("*" + " " * (c- 2) + "*")
+            
